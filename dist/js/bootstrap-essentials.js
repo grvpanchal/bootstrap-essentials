8814e6c7cb19f81ae7451b1b4b1757976be8352d
Gaurav Panchal <gaurav.panchal@sooryen.com>
2016-12-15 14:47:01 +0530

    compiled

--- dist/js/bootstrap-essentials.js
+++ dist/js/bootstrap-essentials.js
@@ -538,7 +538,7 @@
     if (this.state == 'slide-in') {
       var doAnimate = $.support.transition;
 
-      this.$backdrop = $('<div class="modal-backdrop fade" style="z-index:1029" />')
+      this.$backdrop = $('<div class="modal-backdrop navmenu-modal fade" />')
       .insertAfter(this.$element);
 
       if (doAnimate) this.$backdrop[0].offsetWidth // force reflow
