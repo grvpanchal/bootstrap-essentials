160bf671be29bcfa18d524a07965c28f162cad3b
Gaurav Panchal <grvpanchal@gmail.com>
2017-07-10 10:39:17 +0530

    classes added

--- docs/fulldocs/classes.rb
+++ docs/fulldocs/classes.rb
@@ -1,37 +1,37 @@
 require 'csv'
 
-csv = File.read('../_data/classes.csv') 
-ccsv = CSV.parse(csv, headers: true)
-ccsv.each do |row| 
-    print "File Name: #{ row["class-name"].gsub(" ","-").gsub(".","") } \n"
-    print "Title: #{ row["title"] } \n"
-    print "Class Name: #{ row["class-name"] } \n"
-    print "Description: #{ row["description"] } \n"
-    print "Top Category: #{ row["top-category"] } \n"
-    print "Category: #{ row["category"] } \n"
-    print "Origin: #{ row["origin"] } \n"
-    print "Use Cases: #{ row["use-cases"] } \n"
-    print "Chained Classes: #{ row["chained-class"] } \n"
-    print "Parent Classes: #{ row["parent-class"] } \n"
-    print "Element: #{ row["element"] } \n"
-    
-    exit
-    
-end
 
-
-def make_file(slug, title, category, parent = '', indx =false, layout = 'class')
-    
-    indx ? filename = 'index' : filename = slug 
-
-    f= File.new( parent + "/" + filename + ".html",'w')
+def make_file(row, layout = 'class')
+    filename =  row["class-name"].gsub(" ","-").gsub(".","")
+    f= File.new( "classes/" + filename + ".html",'w')
     f << "---
+title: #{ row["title"] }
 layout: #{layout}
-title: #{title}
-slug: #{slug}
-category: #{category}
----
-
-{% include fulldocs/#{parent}/#{filename}.html %}"
+class: #{ row["class-name"] }
+slug: #{filename}
+category: classes
+top-category: #{ row["top-category"] }
+category-slug: #{ row["category"] }
+description: '#{ row["description"] }'
+chained-class: '#{ row["chained-class"] }'
+parent-class: '#{ row["parent-class"] }'
+element: '#{ row["element"] }'
+tags:
+    - #{ row["origin"] }
+    - #{ row["top-category"] }
+---"
      f.close
 end
+
+def generate_files()
+    csv = File.read('../_data/classes.csv') 
+    ccsv = CSV.parse(csv, headers: true)
+    ccsv.each do |row| 
+        print "File Name: #{ row["class-name"].gsub(" ","-").gsub(".","") } \n"
+        
+        make_file(row)
+
+    end 
+end
+
+generate_files()
